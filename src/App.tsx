4a21bd4074b2bb7845d19feb798dd1c4d5d18c7c
vagner da silva <vagnrdaia@gmail.com>
2022-03-05 00:01:57 -0300

    styling component home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from './components/Header'
+import { Home } from './components/Home'
 import './style/app.scss'
 
-export function App(){
+export function App() {
     return (
         <>
-            <Header/>
+            <Header />
+            <Home />
         </>
     )
 }
