f6413615e19bf3a8dd5044d0ad82e83ebdde4965
vagner da silva <vagnrdaia@gmail.com>
2022-03-10 23:12:47 -0300

    component logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import '../style/Logo.scss'
+
+export function Logo() {
+    return (
+        <>
+            <div className="container__header-logo">
+                <div className='image'></div>
+                <h2>Studio<span>Performace</span></h2>
+            </div>
+        </>
+    )
+}
