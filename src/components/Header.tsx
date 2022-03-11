05dd1d7be150a6bf822c57f0d3a32ee600352821
vagner da silva <vagnrdaia@gmail.com>
2022-03-10 23:13:47 -0300

    add components to app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,16 @@
-import { faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NavBar } from './NavBar';
+import { Logo } from './Logo';
+import { SocialMedia } from './SocialMedia';
+
 
 import '../style/Header.scss';
 
 export function Header() {
     return (
         <header className="container__header">
-            <div className="container__header-logo">
-                <div className='image'></div>
-                <h2>Studio<span>Performace</span></h2>
-            </div>
-            <nav className="container__header-nav">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Team</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Prices</a></li>
-                    <li><a href="#">workSpace</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-            <div className="container__header-social__media">
-                <ul>
-                    <li><FontAwesomeIcon icon={faFacebook} /></li>
-                    <li><FontAwesomeIcon icon={faWhatsapp} /> </li>
-                    <li><FontAwesomeIcon icon={faInstagram} /> </li>
-                </ul>
-            </div>
+            <Logo/>
+            <NavBar/>
+            <SocialMedia/>
         </header>
     )
 }
