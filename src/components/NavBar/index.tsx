3b2511d160b17ec1c83285aa20f3b2f043a44cad
vagner da silva <vagnrdaia@gmail.com>
2022-03-25 23:43:03 -0300

    add Link in the component NavBar for navigation

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,16 +1,16 @@
 import './style.scss';
-
+import { Link } from 'react-router-dom'
 export function NavBar() {
     return (
         <>
             <nav className="container__header-nav">
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">workSpace</a></li>
-                    <li><a href="#">Team</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/About">About</Link></li>
+                    <li><Link to="/services">Services</Link></li>
+                    <li><Link to="/workspace">workSpace</Link></li>
+                    <li><Link to="/team">Team</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </>
