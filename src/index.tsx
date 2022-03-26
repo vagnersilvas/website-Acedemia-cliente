356935ea3fa122b12b0ccac6634cf8b52d956904
vagner da silva <vagnrdaia@gmail.com>
2022-03-25 23:48:23 -0300

    add BrowserRouter to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,14 @@
+import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
-render(<App/>, document.getElementById('root'));
+render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+
+    document.getElementById('root')
+);
