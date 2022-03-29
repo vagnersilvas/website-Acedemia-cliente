728eef9d8b05fb1c5b855891dbbb3576023e61a8
vagner da silva <vagnrdaia@gmail.com>
2022-03-28 23:38:49 -0300

    component about

---
+++ src/components/About/index.tsx
@@ -0,0 +1,43 @@
+import './style.scss'
+
+export function About() {
+    return (
+        <>
+            <p>
+                Exercitar o corpo faz bem, o ano todo. É importante para oferecer resistência, flexibilidade aos ossos e articulações.
+                Diminuir as chances de acidentes e doenças. Além disso, aumenta a autoestima e diminui os riscos de depressão.
+                Gostou dos benefícios? Então venha você também fazer parte do time Studio Performance.
+            </p>
+            <p>
+                A Studio Performance entende a importância de você estar satisfeito com o seu corpo e sua saúde não apenas no verão,
+                mas todos os dias, independente da estação. Por este motivo conta com uma série especial de atividades como:
+            </p>
+            <ul>
+                <li>Bungee Dance</li>
+                <li>Cross Corpus</li>
+                <li>Piloxing</li>
+                <li>Ritmos</li>
+                <li>Pilates</li>
+                <li>Alongamento</li>
+                <li>Personal Training</li>
+                <li>Zumba</li>
+                <li>Jump</li>
+                <li>Hit</li>
+                <li>Power local</li>
+                <li>Espaço Funcional</li>
+            </ul>
+
+            <p>
+                Tudo acompanhado por profissionais competentes, que irão garantir a segurança dos seus exercícios
+                e a otimização dos resultados, sob a supervisão de Vivian Bins em uma estrutura apropriada.
+            </p>
+
+
+            <p>
+                Venha fazer uma visita a Studio Performance, estamos te esperando na
+                Rua Alegrete, 126, em Não-Me-Toque, telefone 3332-3114.
+                Curta nossa página no Facebook e acompanhe nossas atividades:
+            </p>
+        </>
+    )
+}
